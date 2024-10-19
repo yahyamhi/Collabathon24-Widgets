3a299b7906e3374b0d55b523eac5ece44a4f5175
Abdul Rahman Rafique <trafique@luxoft.com>
2024-10-19 10:30:13 +0200

    accountSummary backend

--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const branchController = require('../controller/branchController');
+const accountController = require('../controller/accountController');
 const router = express.Router();
 
 router.get('/branches', branchController.getBranches);
+
+// Add the route for account summary
+router.get('/account-summary/:accountId', accountController.getAccountSummary);
 
 module.exports = router;
